67e9e617176d4b978b041734911336d43f762996
Rory Hellier <rory.hellier@googlemail.com>
2020-02-21 19:31:31 +0000

    Edited the rest-requester.rb script to include the user_index action and renamed the actions to be a little clearer' :

--- app/script/rest_requester.rb
+++ app/script/rest_requester.rb
@@ -11,11 +11,13 @@ end
 # user_prompt("What url would you like to search for? ")
 # url = gets.chomp
 url = 'http://localhost:3000'
-show = 'localhost:3000/users/1'
-new_user = 'localhost:3000/users/new'
-edit = 'localhost:3000/users/edit'
+user_index = 'http://localhost:3000/users'
+show_user = 'http://localhost:3000/users/1'
+new_user = 'http://localhost:3000/users/new'
+edit_user = 'http://localhost:3000/users/1/edit'
 
 puts RestClient.get(url)
-puts RestClient.get(show)
+puts RestClient.get(user_index)
+puts RestClient.get(show_user)
 puts RestClient.get(new_user)
-puts RestClient.get(edit)
+puts RestClient.get(edit_user)
